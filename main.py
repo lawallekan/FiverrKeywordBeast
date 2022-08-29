db62381bfc436576a25f6ab2a239944df930109c
lawallekan <lekanlawal83@gmail.com>
2022-08-29 23:21:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+print("This program is create by Lawal Ridwan")
+
+user_name = input("What is your name:")
+print("Hello ", user_name  + " Welcome To Fiverr Keyword Generation")
+
+print("This program is create by Lawal Ridwan")
