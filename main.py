64b06b97f150eb4c1e2d3958aba26aef8f3c8117
lawallekan <lekanlawal83@gmail.com>
2022-08-29 23:48:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
-print("This program is create by Lawal Ridwan")
+#Fiverr Keyword Beast Generator
+import time
+print("This program is create by Lawal Ridwan.")
+
+time.sleep(2)   # Delays for 2 seconds.
 
 user_name = input("What is your name:")
-print("Hello ", user_name  + " Welcome To Fiverr Keyword Generation")
 
-print("This program is create by Lawal Ridwan")
+print("Hello ", user_name  + " Welcome To Fiverr Keyword Generation.")
+
+time.sleep(2)   # Delays for 2 seconds.
+print(""".
+  .
+  .
+  .""")
+
+print(user_name + " You Will Need passcode for access")
+pwd = input("Input passcode: ")
+
+print(""".
+  .
+  .
+  .""")
+
+if pwd == "1234":
+  print(user_name, "Welcome")
+  print(""".
+  .
+  .
+  .""")
+  time.sleep(2)   # Delays for 2 seconds.
+  print("Please input your niche in next prompt")
+  print(""".
+  .
+  .
+  .""")
+  time.sleep(2)   # Delays for 2 seconds.
+  my_list = ["1. Ebook","2. Website design","3. Graphics Design", "4. Youtube", "5. Facebook"] 
+  print('\n'.join(my_list))
+
+
+  
+else:
+  print("Go and get the passcode now")
+  exit()
+
+
+
+
+
