61c46ae5048a62e74fe552ccdfc19c3b7f14ea41
lawallekan <lekanlawal83@gmail.com>
2022-08-30 11:16:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,12 +37,415 @@ if pwd == "1234":
   time.sleep(2)   # Delays for 2 seconds.
   my_list = ["1. Ebook","2. Website design","3. Graphics Design", "4. Youtube", "5. Facebook"] 
   print('\n'.join(my_list))
-
-
-  
+  print("Indicate your pick with number. e.g Ebook is 1 e.t.c")
 else:
   print("Go and get the passcode now")
-  exit()
+  exit() 
+  
+niche = input("What do you pick: ")
+
+if niche == "1":
+    print("""african american children's ebook illustration
+amazon ebook marketing
+amazon ebook promotion
+amazon kdp ebook cover design
+amazon kdp ebook publishing
+amazon kindle ebook promotion
+animated ebook trailer
+awesome children ebook illustrations
+beta ebook reader
+books and ebook writing
+brand ebook
+children ebook
+children ebook cover design
+children ebook illustration
+children ebook writing
+children story ebook illustration
+christian ebook
+christian ebook writer
+comic ebook artist
+comic ebook illustration
+comic ebook writer
+credit repair ebook
+ebook
+ebook author website
+ebook blurb
+ebook cover
+ebook cover design
+ebook creator
+ebook description
+ebook design
+ebook editing and proofreading
+ebook editor
+ebook formatting
+ebook formatting and layout design
+ebook formatting for amazon kdp
+ebook ghostwriter
+ebook illustration
+ebook interior design
+ebook keeping
+ebook landing page
+ebook launch
+ebook layout
+ebook layout design
+ebook marketing
+ebook mockup
+ebook outline
+ebook promotion
+ebook proofreading
+ebook publishing
+ebook reading
+ebook summary
+ebook title
+ebook trailer
+ebook translation
+ebook writer
+fantasy ebook cover
+fitness ebook
+ghost ebook writer
+halloween coloring ebook
+health and fitness ebook
+horror ebook cover
+kdp ebook cover
+kdp ebook formatting
+kdp low content ebook
+kids activity ebook
+kids ebook illustration
+kids ebook illustration and cover
+kids story ebook
+kids story ebook illustration
+kindle ebook cover
+kindle ebook formatting
+kindle ebook promotion
+kindle ebook writer
+low content ebook
+motivational ebook
+organic ebook promotion
+professional ebook cover design
+real estate ebook
+recipe ebook
+romance ebook cover
+romance ebook promotion
+self help ebook
+story ebook illustration
+watercolor children ebook illustration""")
+
+elif niche == "2":
+  print("""build responsive wordpress website design
+build wordpress website design
+business website design
+business wordpress website design
+create beautiful responsive website design
+create responsive wordpress website design
+design or redesign wix website
+design shopify website
+design webflow website
+design wordpress website
+develop responsive wordpress website design
+ecommerce website design
+elementor website design
+godaddy website design
+godaddy website redesign
+graphic design website
+kajabi website design
+modern website design
+modern wordpress website design
+nft website design
+professional wordpress blog website design
+professional wordpress website design
+responsive wordpress website design
+shopify website design
+shopify website design and redesign
+shopify website redesign
+squarespace website design
+squarespace website design and redesign
+squarespace website redesign
+ui website design
+web site designer
+web ui design
+website banner design
+website design figma
+website design godaddy
+website design html css
+website design nft
+website design shopify
+website design squarespace
+website design ui
+website design wix
+website design wordpress
+website designer and developer
+website graphic design
+website logo design
+website redesign
+website ui ux design
+wix website design
+wordpress website design
+wordpress website design and development""")
+
+elif niche == "3":
+  print("""canva graphic design
+clothing graphic design
+custom graphic t-shirt design
+design graphics
+graphic design
+graphic design brochure
+graphic design canva
+graphic design clothing
+graphic design illustrator
+graphic design logos
+graphic design portfolio
+graphic design social media
+graphic design t-shirt
+graphic design website
+graphic t-shirt design
+grapic design
+motion graphics designer
+product graphic design
+professional graphic designer
+social media graphic design
+top rated graphic designer
+website graphic design""")
+
+elif niche == "4":
+  print("""automated youtube
+best youtube video seo
+cash cow youtube
+channel youtube
+create youtube video
+create youtube video intro
+custom youtube intro
+design amazing youtube thumbnail
+design youtube thumbnail
+dmca youtube
+editable canva youtube thumbnail
+faceless youtube
+free youtube thumbnail
+gaming youtube thumbnail
+grow youtube channel
+intro youtube
+keyword research for youtube
+kids youtube promotion
+manage youtube
+manage youtube channel
+meditation youtube videos
+minecraft youtube banner
+minecraft youtube logo
+minecraft youtube thumbnail
+monetize youtube
+monetize youtube channel
+motivational videos for youtube
+organic youtube
+organic youtube growth
+organic youtube video promotion
+professional video editing for youtube
+promote youtube
+promote youtube channel
+promote youtube video
+script writing youtube
+seo youtube
+super fast organic youtube promotion
+thumbnails youtube
+usa youtube channel promotion
+usa youtube music promotion
+usa youtube promotion
+usa youtube video promotion
+video editing for youtube
+video editing for youtube vlog
+watch youtube
+watch youtube videos
+youtube ad video
+youtube ads
+youtube automation
+youtube automation channel
+youtube backlinks
+youtube banner
+youtube banner and logo
+youtube banner art
+youtube banner design
+youtube bot
+youtube channel
+youtube channel create
+youtube channel create and setup
+youtube channel growth
+youtube channel growth manager
+youtube channel intro
+youtube channel logo
+youtube channel manager
+youtube channel marketing
+youtube channel monetization
+youtube channel review
+youtube channel search engine optimization
+youtube content creator
+youtube content writer
+youtube description
+youtube design
+youtube earning
+youtube editor
+youtube end screen
+youtube gaming editor
+youtube gaming intro
+youtube gaming logo
+youtube gaming thumbnail
+youtube gaming video editing
+youtube grow channel
+youtube growth
+youtube header
+youtube hours
+youtube icon
+youtube influencer
+youtube influencer research
+youtube intro
+youtube intro animation
+youtube intro video
+youtube keyword research
+youtube logo
+youtube logo and banner
+youtube logo design
+youtube marketing
+youtube monetisation
+youtube monetization
+youtube monitaization
+youtube niche
+youtube optimization
+youtube organic growth
+youtube outro
+youtube overlay
+youtube profile picture
+youtube promotion
+youtube promotion for channel monetization
+youtube promotion google ads
+youtube promotion organic
+youtube promotion usa
+youtube revenue
+youtube script writer
+youtube search engine optimization
+youtube seo
+youtube seo expert
+youtube setup
+youtube short video download
+youtube shorts
+youtube shorts channel
+youtube shorts editor
+youtube suscribers
+youtube thamnail
+youtube thumbnail
+youtube thumbnail design
+youtube thumbnail gaming
+youtube title
+youtube transcription
+youtube usa promotion
+youtube usa video promotion
+youtube video
+youtube video and channel promotion
+youtube video creation
+youtube video download
+youtube video editing
+youtube video script writer
+youtube video search engine optimization
+youtube video seo
+youtube video seo and promotion
+youtube watch
+youtube watch yours""")
+  
+elif niche == "5":
+  print("""ads facebook
+business manager facebook
+cover facebook
+create facebook business page
+create facebook page
+create professional facebook business page
+design creative facebook ads
+design facebook cover
+dropshipping facebook video ads
+facebook account recover
+facebook account restricted
+facebook ad account restricted
+facebook ad image
+facebook ads
+facebook ads campaign
+facebook ads creative
+facebook ads creative design
+facebook ads design
+facebook ads expert
+facebook ads marketing
+facebook ads vetor
+facebook advertising expert
+facebook and instagram ads
+facebook and instagram ads campaign
+facebook and instagram marketing
+facebook and instagram shop
+facebook ban
+facebook business manager
+facebook business page
+facebook business page create
+facebook business page setup
+facebook campaign
+facebook content creator
+facebook conversion api with gtm
+facebook cover
+facebook cover design
+facebook cover image
+facebook cover photo design
+facebook design
+facebook disabled
+facebook dropshipping
+facebook dropshipping video ads
+facebook follwers
+facebook free marketing
+facebook hacked
+facebook instagram ads
+facebook lead ads
+facebook lead generation
+facebook logo
+facebook marketing
+facebook marketing gig
+facebook marketing manager
+facebook marketplace
+facebook organic marketing
+facebook page
+facebook page create and setup
+facebook page creation
+facebook page growth
+facebook page monetization
+facebook page optimization
+facebook page restricted
+facebook pixel setup
+facebook post design
+facebook post in canva
+facebook promotion
+facebook reels
+facebook reels promotion
+facebook shop
+facebook shop setup
+facebook thumbnail
+facebook traffic
+facebook verification
+facebook verified business manager
+facebook video
+facebook video ads
+facebook virtual assistant
+grow facebook page
+impressive facebook business page
+impressive facebook business page creation
+manage facebook ads
+marketing facebook
+organic facebook marketing
+organic facebook page promotion
+page facebook
+pages facebook manager
+pixel facebook
+real estate facebook ads
+set up facebook ads
+set up facebook business page
+setup facebook business page
+setup facebook page
+shopify facebook video ads
+vector facebook ads
+verified facebook business manager""")
+
+else:
+  print("Thanks For using our Software")
+  
 
 
 
